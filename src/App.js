1df8ba27578664e26c876a738f88f6efd51da28c
Undeflugger <104163210+Undeflugger@users.noreply.github.com>
2023-03-18 22:31:53 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="logo">
+          <a href="#" className="logo">Hello World!</a>
+        </div>
+        <div className="header_nav">
+          <a href="#" className="header_nav_link">Home</a>
+          <a href="#" className="header_nav_link">Product</a>
+          <a href="#" className="header_nav_link">About us</a>
+          <a href="#" className="header_nav_link">Contacts</a>
+        </div>
+      </header>
+      <div className="Main-container">
+        <div className="Nav">
+          <a href="#" className="nav_link">Home</a>
+          <a href="#" className="nav_link">Digital Marketing</a>
+          <a href="#" className="nav_link">WHY CHOOSE US</a>
+          <a href="#" className="nav_link">National top 50 firms</a>
+          <a href="#" className="nav_link">About us</a>
+          <a href="#" className="nav_link">AWARD WINNING</a>
+          <a href="#" className="nav_link">Practice Advice</a>
+          <a href="#" className="nav_link">Business service</a>
+          <a href="#" className="nav_link">Contacts</a>
+        </div>
+        <div className="Container">
+          <h1>Hello World!</h1>
+          <img src={require('./Images/7.jpg')} />
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam id leo in vitae turpis massa sed elementum. Placerat orci nulla pellentesque dignissim enim sit. Lectus nulla at volutpat diam ut venenatis tellus in metus. Sem et tortor consequat id porta nibh venenatis cras sed. Duis tristique sollicitudin nibh sit amet. Ipsum dolor sit amet consectetur adipiscing. Integer eget aliquet nibh praesent tristique. Malesuada fames ac turpis egestas maecenas pharetra convallis. Vel facilisis volutpat est velit egestas dui id ornare arcu. Sed arcu non odio euismod lacinia at quis. Aliquam malesuada bibendum arcu vitae. Tristique senectus et netus et malesuada. Commodo sed egestas egestas fringilla phasellus faucibus. Ut faucibus pulvinar elementum integer enim neque volutpat. At quis risus sed vulputate. Tempor orci eu lobortis elementum nibh tellus molestie nunc. Leo duis ut diam quam nulla porttitor massa id. Enim ut sem viverra aliquet eget sit amet tellus cras. Nibh venenatis cras sed felis eget velit aliquet sagittis id.
+
+            Malesuada nunc vel risus commodo viverra maecenas. Et magnis dis parturient montes nascetur ridiculus mus. Tempor nec feugiat nisl pretium fusce id. Facilisis mauris sit amet massa vitae tortor. Scelerisque fermentum dui faucibus in ornare quam viverra. Varius sit amet mattis vulputate enim nulla aliquet porttitor lacus. Aliquam id diam maecenas ultricies. Felis bibendum ut tristique et egestas. Sed elementum tempus egestas sed sed risus pretium quam. Nisi scelerisque eu ultrices vitae auctor eu augue ut lectus. Lectus sit amet est placerat in egestas erat imperdiet sed. Enim diam vulputate ut pharetra sit amet aliquam. Ut aliquam purus sit amet luctus venenatis lectus magna fringilla. Ut venenatis tellus in metus vulputate eu.
+
+            Eu ultrices vitae auctor eu augue ut lectus arcu. A pellentesque sit amet porttitor eget dolor morbi non arcu. Et magnis dis parturient montes nascetur ridiculus mus. Aliquam eleifend mi in nulla posuere. Purus in mollis nunc sed id semper risus in hendrerit. Nunc sed velit dignissim sodales. Pellentesque habitant morbi tristique senectus et netus et malesuada. Nibh tortor id aliquet lectus. Sed turpis tincidunt id aliquet risus feugiat in ante. Vulputate ut pharetra sit amet aliquam id diam maecenas. Lobortis elementum nibh tellus molestie nunc non blandit massa enim.
+
+            Sollicitudin nibh sit amet commodo. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Sit amet purus gravida quis. Ut venenatis tellus in metus vulputate. Dictumst vestibulum rhoncus est pellentesque elit. Velit euismod in pellentesque massa placerat duis ultricies. Sodales neque sodales ut etiam sit. Malesuada nunc vel risus commodo viverra maecenas accumsan. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Gravida in fermentum et sollicitudin ac orci. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Vitae elementum curabitur vitae nunc sed. Elementum eu facilisis sed odio morbi quis commodo odio. Sit amet cursus sit amet dictum sit amet justo.
+
+            Id leo in vitae turpis massa sed elementum tempus egestas. Consequat semper viverra nam libero justo laoreet. Ac odio tempor orci dapibus ultrices in iaculis nunc. Placerat vestibulum lectus mauris ultrices eros in cursus. Ultricies integer quis auctor elit sed vulputate mi. Sed ullamcorper morbi tincidunt ornare massa. Quis commodo odio aenean sed adipiscing diam. Et leo duis ut diam quam. Pulvinar elementum integer enim neque volutpat ac tincidunt. Aliquam ut porttitor leo a diam sollicitudin tempor. Dolor sit amet consectetur adipiscing elit.
+
+            Elit ut aliquam purus sit amet luctus venenatis lectus. Nunc lobortis mattis aliquam faucibus purus in massa tempor nec. Tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse. Feugiat in ante metus dictum at tempor. Risus feugiat in ante metus dictum. Ante in nibh mauris cursus mattis molestie. Nascetur ridiculus mus mauris vitae. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Facilisi morbi tempus iaculis urna id volutpat lacus. Eu non diam phasellus vestibulum lorem sed. Vehicula ipsum a arcu cursus vitae. Bibendum arcu vitae elementum curabitur vitae. Bibendum arcu vitae elementum curabitur vitae nunc sed. At in tellus integer feugiat scelerisque varius.
+
+            Sit amet venenatis urna cursus eget nunc. Etiam erat velit scelerisque in dictum. Nec ullamcorper sit amet risus nullam eget felis eget. Ultrices sagittis orci a scelerisque purus semper eget duis at. Erat pellentesque adipiscing commodo elit at. Vulputate sapien nec sagittis aliquam malesuada bibendum. Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Quis imperdiet massa tincidunt nunc pulvinar. Scelerisque in dictum non consectetur a erat nam. Egestas egestas fringilla phasellus faucibus. Tortor condimentum lacinia quis vel eros donec ac. Nam at lectus urna duis convallis convallis tellus. Proin fermentum leo vel orci porta non pulvinar. Nulla at volutpat diam ut venenatis tellus. Orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor.
+
+            Et odio pellentesque diam volutpat commodo sed egestas egestas. Eget mauris pharetra et ultrices neque ornare. Magna etiam tempor orci eu lobortis elementum nibh tellus. Non pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus. Amet nisl purus in mollis nunc sed id semper. Malesuada proin libero nunc consequat interdum varius sit amet mattis. Id neque aliquam vestibulum morbi blandit cursus risus. Sed vulputate odio ut enim blandit. Tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dignissim diam quis enim lobortis scelerisque fermentum dui. Lectus sit amet est placerat in egestas. Lorem donec massa sapien faucibus et molestie. Ac turpis egestas maecenas pharetra convallis posuere. Sit amet nisl purus in mollis. Sit amet mauris commodo quis imperdiet massa tincidunt. Nulla porttitor massa id neque aliquam vestibulum. Maecenas ultricies mi eget mauris.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
